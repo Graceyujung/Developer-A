f2a8ebbbc2d0ef8ec617b3e90a210ed495d950b7
Grace Jeong <dbwjd1742@gmail.com>
2024-09-25 07:44:53 +0900

    Update scrip to greet user by name

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # app.py
 def greet(name):
-	return f"Hello, {name}!"
+    return f"Hello, {name}!"
 
 if _name_ == "_main_":
     user_name = input("Enter your name: ")
-	print(greet("World"))
+    print(greet("World"))
